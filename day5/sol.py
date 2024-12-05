9eab1ede02772b6239cd334ee17c5708efed78a6
David Laessker <davidlaessker@gmail.com>
2024-12-05 14:13:18 +0100

    mys

--- day5/sol.py
+++ day5/sol.py
@@ -14,29 +14,31 @@ for line in data:
         continue
     if second: seqs += [[int(x) for x in line.split(",")]]
     else: pairs += [[int(x) for x in line.split("|")]]
-# part 1 ----
 
-valids = []
+# p1 -----
+
+invalids = []
 part1 = 0
 
-for k,seq in enumerate(seqs):
+for seq in seqs:
     for i in range(len(seq)):
         start = seq[i]
-        if any([[following, start] in pairs for following in seq[i+1:]]): break
+        if any([[following, start] in pairs for following in seq[i+1:]]): 
+            invalids += [seq]
+            break
     else: # axel inspo
-        part1 += seq[int(len(seq)/2)]
-        valids += [k]
+        part1 += seq[len(seq)//2]
 
 print(part1)
 
-# bubble sort the arrays ------ p2
+# p2 bubble sort ;) -----
 
-new_order = []
 import copy
+part2 = 0
 
-for seq in seqs:
+for seq in invalids:
     old_seq = copy.copy(seq)
-    while True:
+    while True: # ;)
         for i in range(len(seq)-1):
             if [seq[i+1],seq[i]] in pairs:
                 temp = seq[i+1]
@@ -44,10 +46,6 @@ for seq in seqs:
                 seq[i] = temp
         if seq == old_seq: break
         else: old_seq = copy.copy(seq)
-    new_order += [seq]
-part2 = 0
-
-for i, seq in enumerate(new_order):
-    if i not in valids: part2 += seq[int(len(seq)/2)]
+    part2 += seq[len(seq)//2]
 
 print(part2)
